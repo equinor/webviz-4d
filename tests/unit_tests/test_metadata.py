9cce58b84e6474a66440f6e798ed5cd70ae6267a
ashildskalnes <ashska@equinor.com>
2022-01-26 12:04:19 +0100

    Update of tests (#69)

--- tests/unit_tests/test_metadata.py
+++ tests/unit_tests/test_metadata.py
@@ -1,38 +1,57 @@
 import os
 import pytest
-
-import webviz_4d._datainput.common as common
+import pandas as pd
 import webviz_4d._datainput._metadata as metadata
 
+realizations = ["realization-0", "realization-1", "realization-1"]
+ensembles = ["iter-0", "iter-0", "iter-1"]
+map_types = ["observed", "simulated", "simulated"]
+names = ["all", "zone1", "zone2"]
+attributes = ["4d_diff_rms", "4d_diff_min", "4d_diff_max"]
+statistics = ["", "", "mean"]
+time1 = ["2018-10-01", "2019-10-01", "2020-10-01"]
+time2 = ["2019-10-01", "2020-10-01", "2021-10-01"]
 
-# def test_compose_filename():
-#     config_file = "./tests/data/example_config.yaml"
-#     config = common.read_config(config_file)
+meta_df = pd.DataFrame()
+meta_df["fmu_id.realization"] = realizations
+meta_df["fmu_id.ensemble"] = ensembles
+meta_df["map_type"] = map_types
+meta_df["data.name"] = names
+meta_df["content"] = attributes
+meta_df["data.time.t1"] = time1
+meta_df["data.time.t2"] = time2
+meta_df["statistics"] = statistics
 
-#     shared_settings = config["shared_settings"]
+folder = "/test"
+ext = ".gri"
+filenames = []
 
-#     real = "realization-0"
-#     ensemble = "iter-0"
-#     map_type1 = "observed"
-#     map_type2 = "simulated"
-#     name = "all"
-#     attribute = "4d_diff_rms"
-#     interval = "2020-10-01-2019-10-01"
-#     delimiter = "--"
+for i in range(0, len(realizations)):
+    filename = (
+        folder
+        + "/"
+        + realizations[i]
+        + "/"
+        + ensembles[i]
+        + "/share/observations/maps/"
+        + names[i]
+        + "--"
+        + attributes[i]
+        + "--"
+        + time1[i].replace("-", "")
+        + "_"
+        + time2[i].replace("-", "")
+        + ext
+    )
+    filenames.append(filename)
+    print(filename)
 
-#     filename1 = metadata.compose_filename(
-#         shared_settings, real, ensemble, map_type1, name, attribute, interval, delimiter
-#     )
+meta_df["filename"] = filenames
 
-#     filename2 = metadata.compose_filename(
-#         shared_settings, real, ensemble, map_type2, name, attribute, interval, delimiter
-#     )
 
-#     assert (
-#         filename1
-#         == "/scratch/example/fmu_case/realization-0/iter-0/share/observations/maps/all--4d_diff_rms--20201001_20191001.gri"
-#     )
-#     assert (
-#         filename2
-#         == "/scratch/example/fmu_case/realization-0/iter-0/share/results/maps/all--4d_diff_rms--20201001_20191001.gri"
-#     )
+def test_get_metadata():
+    ens = metadata.get_ensembles(meta_df, "observed")
+    assert ens == ["iter-0"]
+
+    ens = metadata.get_ensembles(meta_df, "simulated")
+    assert ens == ["iter-0", "iter-1"]
