d535c1b04558d6b283119942f14f1deb692fe3b2
ashildskalnes <ashska@equinor.com>
2022-01-26 12:24:47 +0100

    Add PRM receiver lines as a possible map layer (#70)

--- webviz_4d/_datainput/_polygons.py
+++ webviz_4d/_datainput/_polygons.py
@@ -3,28 +3,47 @@ import pandas as pd
 import math
 import glob
 import json
+from pathlib import Path
 
 from webviz_config.webviz_store import webvizstore
 from webviz_config.common_cache import CACHE
 
 import webviz_4d._datainput.common as common
 
+from webviz_4d.plugins._surface_viewer_4D._webvizstore import (
+    read_csv,
+    read_csvs,
+    find_files,
+    get_path,
+)
 
-supported_polygons = {"owc_outline": "OWC", "goc_outline": "GOC", "faults": "Faults"}
+
+supported_polygons = {
+    "owc_outline": "Initial OWC",
+    "goc_outline": "Initial GOC",
+    "faults": "Faults",
+    "prm_receivers": "PRM receivers",
+}
 default_colors = {
     "owc_outline": "lightslategray",
     "goc_outline": "red",
     "faults": "gray",
+    "prm_receivers": "darkgray",
 }
-contact_tooltips = {"OWC": "Initial OWC", "GOC": "Initial GOC"}
+
+checked = {
+    "Initial OWC": True,
+    "Initial GOC": True,
+    "Faults": True,
+    "PRM receivers": False,
+}
 
 key_list = list(supported_polygons.keys())
 val_list = list(supported_polygons.values())
 
 
-def get_fault_position_data(polyline):
+def get_position_data(polyline):
     """Return x- and y-values for a selected polygon"""
-
     positions_txt = polyline["coordinates"]
     positions = json.loads(positions_txt)
 
@@ -33,13 +52,10 @@ def get_fault_position_data(polyline):
 
 def get_fault_polyline(fault, tooltip, color):
     """Create polyline data - fault polylines, color and tooltip"""
-
-    # tooltip = "SEG I.D. " + str(fault["SEG I.D."])
-
     if color is None:
         color = default_colors.get("faults")
 
-    positions = get_fault_position_data(fault)
+    positions = get_position_data(fault)
 
     if positions:
         return {
@@ -50,10 +66,30 @@ def get_fault_polyline(fault, tooltip, color):
         }
 
 
-def get_contact_polyline(contact, label, color):
+def get_prm_polyline(prm, color):
+    """Create polyline data - prm receiver polylines, color and tooltip"""
+    if color is None:
+        color = default_colors.get("prm_receivers")
+
+    positions = get_position_data(prm)
+    year = prm["year"]
+    line = prm["line"]
+    tooltip = str(year) + "-line " + str(line)
+
+    if positions:
+        return {
+            "type": "polyline",
+            "color": color,
+            "positions": positions,
+            "tooltip": tooltip,
+        }
+
+
+def get_contact_polyline(contact, key, color):
     """Create polyline data - owc polyline, color and tooltip"""
     data = []
-    tooltip = contact_tooltips[label]
+    tooltip = supported_polygons[key]
+    label = supported_polygons[key]
 
     coordinates_txt = contact["coordinates"][0]
     coordinates = json.loads(coordinates_txt)
@@ -79,22 +115,36 @@ def get_contact_polyline(contact, label, color):
     return data
 
 
-def make_new_polyline_layer(dataframe, label, color):
+def make_new_polyline_layer(dataframe, key, label, color):
     """Make layeredmap fault layer"""
     data = []
+    name = supported_polygons.get(key)
 
-    if label in contact_tooltips.keys():
-        data = get_contact_polyline(dataframe, label, color)
-        name = label
-    else:
+    if "outline" in key:
+        data = get_contact_polyline(dataframe, key, color)
+    elif key == "prm_receivers":
         for _index, row in dataframe.iterrows():
-            polyline_data = get_fault_polyline(row, label, color)
-            name = "Faults"
+            polyline_data = get_prm_polyline(row, color)
 
             if polyline_data:
                 data.append(polyline_data)
+    else:
+        for _index, row in dataframe.iterrows():
+            polyline_data = get_fault_polyline(row, label, color)
+
+            if polyline_data:
+                data.append(polyline_data)
+    # else:
+    #     print("WARNING: Unknown polygon type", key)
+
     if data:
-        layer = {"name": name, "checked": True, "base_layer": False, "data": data}
+        checked_state = checked.get(name)
+        layer = {
+            "name": name,
+            "checked": checked_state,
+            "base_layer": False,
+            "data": data,
+        }
     else:
         layer = None
 
@@ -107,9 +157,9 @@ def load_polygons(csv_files, polygon_colors):
     for csv_file in csv_files:
         polygon_df = pd.read_csv(csv_file)
         name = polygon_df["name"].unique()[0]
-        if name in supported_polygons.keys():
+        file_name = os.path.basename(csv_file)
 
-            label = supported_polygons.get(name)
+        if name in supported_polygons.keys() or file_name in supported_polygons.keys():
             default_color = default_colors.get(name)
 
             if polygon_colors:
@@ -117,7 +167,7 @@ def load_polygons(csv_files, polygon_colors):
             else:
                 color = default_color
 
-            polygon_layer = make_new_polyline_layer(polygon_df, label, color)
+            polygon_layer = make_new_polyline_layer(polygon_df, name, name, color)
             polygon_layers.append(polygon_layer)
 
     return polygon_layers
@@ -138,7 +188,7 @@ def load_zone_polygons(csv_files, polygon_colors):
         else:
             color = default_color
 
-        polygon_layer = make_new_polyline_layer(polygon_df, label, color)
+        polygon_layer = make_new_polyline_layer(polygon_df, name, label, color)
         polygon_layers.append(polygon_layer)
 
     return polygon_layers
